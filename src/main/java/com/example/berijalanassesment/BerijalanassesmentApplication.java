f9654886eb6aa08c394eb020d57959d1523a4010
Muhamad Daffa Azfa Rabbani <109969988+Asoatram@users.noreply.github.com>
2026-03-27 08:46:07 +0700

    chore: bootstrap Spring Boot project, build tooling, and environment config

---
+++ src/main/java/com/example/berijalanassesment/BerijalanassesmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.berijalanassesment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerijalanassesmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BerijalanassesmentApplication.class, args);
+	}
+
+}
